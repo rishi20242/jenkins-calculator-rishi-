a6b961ae1ee2c8220d69d85f03e90684a2540da5
rishi20242 <rishieaswaran2004@gmail.com>
2026-06-30 21:11:41 +0530

    Revert "Update Calculator.java"
    This reverts commit f807c67f43a83be8aec5be08c22790ba7c79f8c7.

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -2,11 +2,11 @@ package com.example.calculator;
 
 public class Calculator {
     public int add(int a, int b) {
-        return a - b;
+        return a  b;
     }
 
     public int subtract(int a, int b) {
-        return a + b;
+        return a  b;
     }
 
     public int multiply(int a, int b) {
